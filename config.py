30ac4852f36d49403c030c9e274246e07efa0883
alphonce-otieno-odhiambo <alphonce.odhiambo@student.moringaschool.com>
2021-11-15 08:47:00 +0300

    manage procfile

--- config.py
+++ config.py
@@ -1,14 +1,27 @@
-
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRETE_KEY')
-    SQLALCHEMY_DATABASE_URI = 'postgresql + psycopg2://alphonce:alphonce@localhost/sql'
+
+    
+    SECRET_KEY = os.environ.get('something only you know')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://alphonce:alphonce@localhost/sql'
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+#  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 
 class ProdConfig(Config):
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    if SQLALCHEMY_DATABASE_URI and SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
+    
+
 
 class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://alphonce:alphonce@localhost/sql'
     DEBUG = True
 
 config_options = {
---
+++ start.sh
@@ -0,0 +1,6 @@
+export SECRET_KEY=54643923b4beceab8d03f04207070e7f
+
+export MAIL_USERNAME=odhiamboalphonce23@gmail.com
+export MAIL_PASSWORD=welcometomoringaschool01#
+
+python3.8 manage.py server
