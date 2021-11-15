fe59852948fd82e58b9c6edc3a1f1856ca084f43
alphonce-otieno-odhiambo <alphonce.odhiambo@student.moringaschool.com>
2021-11-15 07:35:37 +0300

    Create LoginForm and Validators

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,19 +1,24 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,PasswordField,SubmitField,BooleanField,ValidationError
-from wtforms.validators import Required, Length, Email, EqualTo
+from wtforms import StringField,PasswordField,SubmitField
+from wtforms.validators import Required,Email,EqualTo
 from ..models import User
-
+from wtforms import ValidationError,StringField,PasswordField,BooleanField,SubmitField
 
 class RegistrationForm(FlaskForm):
-    username = StringField('username..', validators=[Required()])
-    email = StringField('email...',validators=[Required(), Email()])
-    password = PasswordField('Enter password...',validators=[Required(),Length(min=6, max=15)])
-    confirm_psw = PasswordField('confirm pass...',validators=[Required(),EqualTo(password)])
-    submit = SubmitField('Sign up')
-
+    email = StringField('Your Email Address',validators=[Required(),Email()])
+    username = StringField('Enter your username',validators = [Required()])
+    password = PasswordField('Password',validators = [Required(), EqualTo('password_confirm',message = 'Passwords must match')])
+    password_confirm = PasswordField('Confirm Passwords',validators = [Required()])
+    submit = SubmitField('Sign Up')
     def validate_email(self,data_field):
             if User.query.filter_by(email =data_field.data).first():
                 raise ValidationError('There is an account with that email')
+
     def validate_username(self,data_field):
         if User.query.filter_by(username = data_field.data).first():
             raise ValidationError('That username is taken')
+class LoginForm(FlaskForm):
+    email = StringField('Your Email Address',validators=[Required(),Email()])
+    password = PasswordField('Password',validators =[Required()])
+    remember = BooleanField('Remember me')
+    submit = SubmitField('Sign In')        
