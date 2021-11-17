f4ae6d23e0ca1782e8764cf23c759dc5d82419a6
alphonce-otieno-odhiambo <alphonce.odhiambo@student.moringaschool.com>
2021-11-17 11:35:50 +0300

    second deployment

--- start.sh
+++ start.sh
@@ -3,6 +3,6 @@ export SECRET_KEY='54643923b4beceab8d03f04207070e7f'
 export MAIL_USERNAME='odhiamboalphonce23@gmail.com'
 export MAIL_PASSWORD='welcometomoringaschool01#'
 
-export  DATABASE_URL = 'postgresql+psycopg2://alphonce:alphonce@localhost/sql'
+ 
 
 python3.8 manage.py server
